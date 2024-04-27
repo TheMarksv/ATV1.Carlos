ef503cf583f20c24f054e822e5438df028c77703
TheMarksv <160982522+TheMarksv@users.noreply.github.com>
2024-04-27 19:03:55 -0300

    Create ATV1CARLOS.py

---
+++ ATV1CARLOS.py
@@ -0,0 +1,8 @@
+# Pasta: ATV1CARLOS
+soma = 0
+
+for i in range(1, 11):
+    numero = int(input("Digite o " + str(i) + "º número inteiro:"))
+    soma += numero
+
+print("A soma dos 10 números é:", soma)
